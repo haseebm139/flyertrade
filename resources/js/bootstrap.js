d7889f7eaa05cc5b13a237d6b416784d3e7a23f9
haseebm139 <haseebm139@gmail.com>
2025-09-03 22:51:05 +0500

    add starter kit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,4 +9,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allow your team to quickly build robust real-time web applications.
  */
 
-import './echo';
+// import './echo';
+// import Echo from 'laravel-echo';
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST ?? '127.0.0.1',
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//     forceTLS: false,
+// });
--- routes/auth.php
+++ routes/auth.php
@@ -1,37 +1,59 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Auth\AuthController;
-use App\Http\Controllers\Api\HomeController;
 
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
+    Route::post('register', [RegisteredUserController::class, 'store']);
 
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
 
-    Route::prefix('auth')->controller(AuthController::class)->group(function () {
-        Route::post('register','register');
-        Route::post('login', 'login');
-        Route::post('guest', 'guestLogin');
-        Route::post('{proverder}/login','socialLogin');
-        Route::post('facebook_login', 'facebookLogin');
-        Route::post('apple_login', 'appleLogin');
-        Route::post('send-code-to-email', 'sendCodeToEmail');
-        Route::post('update-password', 'updatePassword');
-    });
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('location', [AuthController::class,'updateLocation']);
-        Route::post('logout', [AuthController::class,'logout']);
-        Route::post('/change-password', [AuthController::class,'changePassword']);
-    });
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->name('password.request');
 
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
 
-        Route::controller(HomeController::class)->group(function () {
-            Route::get('/services', 'services');
-        });
-    });
-    Route::middleware(['auth:sanctum', 'role:customer'])->get('/customer/profile', [CustomerController::class, 'profile']);
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->name('password.reset');
 
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+        ->name('password.store');
+});
 
-    Route::middleware(['auth:sanctum', 'role:provider'])->get('/provider/profile', [ProviderController::class, 'profile']);
+Route::middleware('auth')->group(function () {
+    Route::get('verify-email', EmailVerificationPromptController::class)
+        ->name('verification.notice');
+
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+        ->middleware(['signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware('throttle:6,1')
+        ->name('verification.send');
+
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+        ->name('password.confirm');
+
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+});
