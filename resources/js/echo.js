f216ec4780b43f00d5f4291fef6c5fad46c44b40
haseebm139 <haseebm139@gmail.com>
2025-09-23 07:07:04 +0500

    add auth

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,16 +1,25 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
 // Pusher.logToConsole = true;           // add
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: 80,
-  wssPort: 443,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME || 'https') === 'https',
-  enabledTransports: ['ws', 'wss'],
-  disableStats: true,                 // add
+    broadcaster: "reverb",
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: 80,
+    wssPort: 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME || "https") === "https",
+    enabledTransports: ["ws", "wss"],
+    disableStats: true, // add
+    authEndpoint: "/broadcasting/auth",
+    withCredentials: true,
+    auth: {
+        headers: {
+            "X-CSRF-TOKEN": document
+                .querySelector('meta[name="csrf-token"]')
+                ?.getAttribute("content"),
+        },
+    },
 });
