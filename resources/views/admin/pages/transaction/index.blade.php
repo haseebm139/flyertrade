7ca730c4dc2004bd5f8854368c75d5e7b9e480ae
muhammadjan761 <muhammadjan761@gmail.com>
2025-11-25 16:40:35 -0800

    Fixed storage issues and updated config

--- resources/views/admin/pages/transaction/index.blade.php
+++ resources/views/admin/pages/transaction/index.blade.php
@@ -609,7 +609,7 @@
 
     <div class="modal-content filter-modal" id="filter-theme-modal-content">
           <div class="modal_heaader">
-                <span class="close-modal"  wire:click="closeFilterModal" >
+                <span class="close-modal" id="closeFilterModal"  wire:click="closeFilterModal" >
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0.75 11.236L5.993 5.993L11.236 11.236M11.236 0.75L5.992 5.993L0.75 0.75" stroke="#717171" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
