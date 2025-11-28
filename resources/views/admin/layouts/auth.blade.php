9375122d65fa436def321fa190196421b0fd6fcb
haseebm139 <haseebm139@gmail.com>
2025-11-28 05:34:18 +0500

    update sign page

--- resources/views/admin/layouts/auth.blade.php
+++ resources/views/admin/layouts/auth.blade.php
@@ -4,30 +4,14 @@
 <head>
 
     <meta charset="utf-8">
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-    >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="{{ asset('assets/logos/favicon.ico') }}">
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossorigin="anonymous"
-    >
-    <link
-        rel="stylesheet"
-        href="{{ asset('assets/css/authentication-style.css') }}"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/css/authentication-style.css') }}">
 
-    <link
-        href="https://fonts.cdnfonts.com/css/clash-display"
-        rel="stylesheet"
-    >
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-    >
+    <link href="https://fonts.cdnfonts.com/css/clash-display" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
 
     <title>{{ $title ?? 'Auth' }}</title>
@@ -42,10 +26,8 @@
             <div class="row row-about">
                 <!-- Left Image -->
                 <div class="col-md-6 left-img">
-                    <img
-                        src="{{ asset('assets/images/icons/authentication-foam-image.svg') }}"
-                        alt="img"
-                    >
+                    <img src="{{ asset('assets/images/icons/authentication-foam-image.svg') }}"
+                        alt="Authentication Illustration" loading="lazy">
                 </div>
 
                 <!-- Right Login Form -->
@@ -58,11 +40,9 @@
         </div>
     </div>
 
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"
-    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
