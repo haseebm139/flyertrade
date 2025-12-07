593d35cee171d2fda7e129974227d92aec6c720e
muhammadjan761 <muhammadjan761@gmail.com>
2025-12-07 12:16:12 -0800

    Fixed storage issues and updated config

--- resources/views/livewire/admin/service-categories/user-providers-modal.blade.php
+++ resources/views/livewire/admin/service-categories/user-providers-modal.blade.php
@@ -3,7 +3,7 @@
         <div class="modal " style="display: flex;" id="userModal" tabindex="-1" aria-labelledby="userModalLabel"
             aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered jusoio">
-                <div class="modal-content  rounded-3">
+                <div class="modal-content  rounded-3" style="position:static">
                     <div class="modal-header border-0">
                         <h5 class="modal-title moddal" id="userModalLabel">{{ $title }}</h5>
                         <button type="button" wire:click="closeModal" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -17,8 +17,8 @@
                                     <img src="{{ asset($provider->avatar ?? 'assets/images/icons/person-one.svg') }}" class="rounded-circle me-3"
                                         style="width:35px; height:35px; object-fit:cover;" alt="User">
                                     <div>
-                                        <h6 class="mb-0">{{ $provider->name }}</h6>
-                                        <small class="text-muted">{{ $provider->email ?? '' }}</small>
+                                        <h6 class="mb-0" style="font-size:0.833vw;">{{ $provider->name }}</h6>
+                                        <small class="text-muted" style="font-size:0.729vw;">{{ $provider->email ?? '' }}</small>
                                     </div>
                                 </div>
                             @empty
